a9648376f3e25f9f36635e5d11802c063343f260
wangsq <wangsq@tongtech.com>
2021-01-22 15:17:48 +0800

    update

--- controllers/tlqmaster_controller.go
+++ controllers/tlqmaster_controller.go
@@ -39,7 +39,7 @@ type TLQMasterReconciler struct {
 //+kubebuilder:rbac:groups=tlq.tongtech.com,resources=tlqmasters/finalizers,verbs=update
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="apps",resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="",resources=service,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
